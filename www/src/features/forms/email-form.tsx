b34111dd6f3f33b9aec364b38a7e67ed5a057a1e
Simone Bergamin <simone.bergamin@crispybacon.it>
2024-02-22 10:02:19 +0100

    fix(email form): add missing required parts for Netifly
    Ref: #AWS-7

--- www/src/features/forms/email-form.tsx
+++ www/src/features/forms/email-form.tsx
@@ -28,7 +28,7 @@ const EmailForm = () => {
 
 
     return (
-        <form onSubmit={handleSubmit} className="flex justify-center items-center" name="get-news-form"
+        <form onSubmit={handleSubmit} className="flex justify-center items-center" name="contact"
               data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact"/>
             <Input
